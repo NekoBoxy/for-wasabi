f6351b6ea53ab9cbec7a0fda619013aa2876d348
CatBoxy <catboxy@gmail.com>
2023-05-10 17:09:38 +0800

    list 建置中

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,7 +1,42 @@
-<template></template>
+<template>
+  <CTabs />
+  <table>
+    <tr>
+      <td>name</td>
+      <td>gender</td>
+      <td>job</td>
+    </tr>
+    <tr v-for="people in peoples" :key="people.name" class="td-style">
+      <td>{{ people.name }}</td>
+      <td>{{ people.gender }}</td>
+      <td>{{ people.job }}</td>
+    </tr>
+  </table>
+</template>
 
 <script>
-
+import CTabs from '../components/CTabs.vue';
+export default {
+  data() {
+    return {
+      peoples: [
+        {
+          name: "john",
+          gender: "m",
+          job: 1,
+        },
+        {
+          name: "mary",
+          gender: "f",
+          job: 2,
+        },
+      ]
+    }
+  },
+  components: {
+    CTabs,
+  },
+}
 </script>
 
 
